064cb034557ccbe2d70fa414837df4ca80655fa5
Gabriel Campista <gcampss@gmail.com>
2020-12-07 17:15:15 +0000

    rollbacking

--- db/migrate/20201205150006_create_posts.rb
+++ db/migrate/20201205150006_create_posts.rb
@@ -2,8 +2,10 @@ class CreatePosts < ActiveRecord::Migration[6.0]
   def change
     create_table :posts do |t|
       t.string :description
-
+      t.string :title
+      t.references :category, foreign_key: true
       t.timestamps
+
     end
   end
 end
