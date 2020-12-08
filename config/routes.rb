45ccd361ed5009c827abb0ae53e1559b97942daf
Gabriel Campista <gcampss@gmail.com>
2020-12-08 18:23:20 +0000

    adding more stuff

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
   #get 'categories/pages'
   devise_for :users
-  root to: 'categories#index'
-  resources :categories, except: :new do
-  end
-  resources :posts, only: [:show, :create, :destroy] do
-  end
+  root to: 'pages#home'
+  resources :categories 
+
+  resources :posts
+  
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
