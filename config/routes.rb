751c9031fd2ffdb0b39179a75f899e44a7c7d6da
Gabriel Campista <gcampss@gmail.com>
2020-12-08 15:45:07 +0000

    routes controller and views 1.0

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
-  get 'categories/pages'
+  #get 'categories/pages'
   devise_for :users
-  root to: 'pages#home'
+  root to: 'categories#index'
+  resources :categories, except: :new do
+  end
+  resources :posts, only: [:show, :create, :destroy] do
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
