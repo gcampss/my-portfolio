7f3d4cbf4429aa1f365d0df5835280abd56792b5
Gabriel Campista <gcampss@gmail.com>
2020-12-10 14:25:27 +0000

    controller new and form categoy

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,10 +1,21 @@
 class CategoriesController < ApplicationController
     
     def index
-    @categories = Category.all
+      @categories = Category.all
     end
 
     def show
-    @categories = Category.find(params[:id])
+      @categories = Category.find(params[:id])
+    end
+
+    def new
+    @category = Category.new
+    end
+    
+    def create
+        @categories = Category.find(params[:id])
+        @categories = Category.new 
     end
 end
+
+
