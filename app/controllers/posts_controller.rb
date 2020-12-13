87eb668787421d7e557bf3baa10de053ee8da764
Gabriel Campista <gcampss@gmail.com>
2020-12-13 02:27:16 +0000

    creating post controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,24 +1,39 @@
 class PostsController < ApplicationController
-
+before_action :set_post, only: [:edit, :update, :destroy]
 
   def new
+    @post = Post.new
   end
 
   def create
+    @post = Post.new(set_post)
+    @post.save
   end
 
   def edit
   end
 
   def update
+    if @post.update(post_params)
+      redirect_to category_path
+    else
+      render :edit
   end
 
   def destroy
+    @post = Post.find(params[:id])
+    @post.destroy!
+
+    redirect_to categories_path
   end
 
   private
 
   def post_params
-    params.require(:post).permit(:title, :description, :photo)
+    params.require(:post).permit(:title, :description, :photos)
+  end
+
+  def set_post
+    @post = Post.find(params[:id])
   end
 end
