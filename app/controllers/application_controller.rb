f89c42f5cc49ada73afe8586397e82f1d2819c28
Gabriel Campista <gcampss@gmail.com>
2021-01-22 17:34:59 +0000

    meta ok

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,3 +2,9 @@ class ApplicationController < ActionController::Base
   # method that comes with devise that check if the user is logged in
     before_action :authenticate_user!
 end
+
+# app/controllers/application_controller.rb
+
+def default_url_options
+  { host: ENV["DOMAIN"] || "localhost:3000" }
+end
